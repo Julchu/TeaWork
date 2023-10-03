78c0a8184c8283774ab96d5eea07eeab3b7243bd
julchu <jchumtl@gmail.com>
2023-10-02 20:51:39 -0400

    Adding Prettier and cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
-import { Button } from "app/components/ui/button"
-import { FC } from "react"
+import { Button } from "app/components/ui/button";
+import { FC } from "react";
 
 const Home: FC = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Click me</Button>
-    
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
