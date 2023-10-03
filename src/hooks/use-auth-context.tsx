4fceb5098f75ecfec232172b78b8c095698eba59
julchu <jchumtl@gmail.com>
2023-10-03 16:58:09 -0400

    Typo

--- src/hooks/use-auth-context.tsx
+++ src/hooks/use-auth-context.tsx
@@ -3,7 +3,7 @@ import { FC, ReactNode } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { authentication } from 'src/lib/firebase/setup';
 import Login from 'src/components/auth/login';
-import Signout from 'src/components/auth/sign-out';
+import SignOut from 'src/components/auth/sign-out';
 
 const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [user, loading, error] = useAuthState(authentication);
@@ -14,7 +14,7 @@ const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
 
   return (
     <>
-      <Signout />
+      <SignOut />
       {children}
     </>
   );
