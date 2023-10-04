da650d393874ec5c7e577bb65658eaba3285cb8e
julchu <jchumtl@gmail.com>
2023-10-04 00:08:49 -0400

    Adding Mapbox styles

--- src/components/test/map-test.tsx
+++ src/components/test/map-test.tsx
@@ -11,16 +11,34 @@ const MapTest: FC = () => {
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
   const [zoom, setZoom] = useState(9);
-  //
+
   useEffect(() => {
     if (map.current) return;
     mapBoxGL.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN || '';
     map.current = new mapBoxGL.Map({
+      attributionControl: false,
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v12',
+      // style: 'mapbox://styles/mapbox/basic-v8',
+      // style: 'mapbox://styles/mapbox/satellite-v8',
+      // style: 'mapbox://styles/mapbox/bright-v8',
       center: [lng, lat],
       zoom: zoom,
-    });
+    })
+      .addControl(
+        new mapBoxGL.AttributionControl({
+          compact: true,
+        }),
+      )
+      .addControl(
+        new mapBoxGL.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true,
+          },
+          trackUserLocation: true,
+          showUserHeading: true,
+        }),
+      );
   });
 
   return (
