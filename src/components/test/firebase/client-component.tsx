9d3cab6991e10c3438d4586219320a88eccd9534
julchu <jchumtl@gmail.com>
2023-10-05 17:11:09 -0400

    Updating client-component.tsx to realtime

--- src/components/test/firebase/client-component.tsx
+++ src/components/test/firebase/client-component.tsx
@@ -1,22 +1,28 @@
 'use client';
-import React, { FC, useEffect, useState } from 'react';
-import { getAllData } from 'src/app/api/firestore';
+import React, { FC, useEffect } from 'react';
 import { GenericsUser } from 'src/lib/firebase/interfaces/generics';
+import { useStreamAllData } from 'src/app/api/firestore-hooks';
 
-const ClientComponent: FC = () => {
-  const [data, setData] = useState<GenericsUser[]>([]);
+const ClientComponent: FC<{ initialData?: GenericsUser[] }> = ({ initialData }) => {
+  // const [data, setData] = useState<GenericsUser[]>([]);
+  const { users, getFirestoreData } = useStreamAllData();
+
+  // useEffect(() => {
+  //   const goFetch = async () => {
+  //     // setData(JSON.stringify(await getData('02wUnDjap2L7deE6ySu3')));
+  //     setData(await getAllData());
+  //   };
+  //
+  //   goFetch();
+  // }, []);
 
   useEffect(() => {
-    const goFetch = async () => {
-      // setData(JSON.stringify(await getData('02wUnDjap2L7deE6ySu3')));
-      setData(await getAllData());
-    };
-
-    goFetch();
-  }, []);
+    getFirestoreData();
+  }, [getFirestoreData]);
 
   // return <div className={'bg-red-900'}>{data}</div>;
-  return <div className={'bg-green-800'}>{data.map(user => JSON.stringify(user))}</div>;
+  // return <div className={'bg-green-800'}>{data.map(user => JSON.stringify(user))}</div>;
+  return <div className={'bg-green-800'}>{users?.map(user => JSON.stringify(user))}</div>;
 };
 
 export default ClientComponent;
