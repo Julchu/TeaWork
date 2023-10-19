5285c28bb2aaab4bb1b8c4f8dc9f630efa783526
julchu <jchumtl@gmail.com>
2023-10-19 03:32:37 -0400

    Fixing auth provider

--- src/hooks/use-auth-context.tsx
+++ src/hooks/use-auth-context.tsx
@@ -16,29 +16,6 @@ export const useAuthContext = (): AuthProps => useContext(AuthContext);
 
 const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [authUser, userLoading, userError] = useAuthState(authentication);
-  const [authUser, userLoading, userError] = useAuthState(authentication, {
-    onUserChanged: async userCredentials => {
-      if (!userCredentials) {
-        const response = await fetch('/api/signOut', {
-          method: 'POST',
-        });
-
-        if (response.status === 200) {
-          console.log('not logged in');
-        }
-      } else {
-        fetch('/api/login', {
-          method: 'POST',
-          headers: {
-            Authorization: `Bearer ${await userCredentials.getIdToken()}`,
-          },
-        }).then(async response => {
-          console.log(await userCredentials.getIdToken());
-          console.log('response', response);
-        });
-      }
-    },
-  });
 
   return (
     <AuthContext.Provider value={{ authUser, userLoading, userError }}>
