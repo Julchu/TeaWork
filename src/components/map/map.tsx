f55d66caae25610d3b74c7bb95eabe9320af1f9d
julchu <jchumtl@gmail.com>
2023-10-21 17:30:22 -0400

    Adding markers

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -3,7 +3,7 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import "./map.css";
 import mapBoxGL, { LngLatLike } from "mapbox-gl";
 import * as React from "react";
-import { FC, useCallback, useEffect, useRef, useState } from "react";
+import { FC, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { Button } from "src/components/ui/button";
 import * as process from "process";
 import Spinner from "src/components/ui/spinner";
@@ -91,6 +91,39 @@ const Map: FC<{
     });
   }, [updateUserLocation]);
 
+  const htmlElement = useMemo(() => {
+    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-5 h-5 animate-ping">
+        <path fill-rule="evenodd" d="M9.69 18.933l.003.001C9.89 19.02 10 19 10 19s.11.02.308-.066l.002-.001.006-.003.018-.008a5.741 5.741 0 00.281-.14c.186-.096.446-.24.757-.433.62-.384 1.445-.966 2.274-1.765C15.302 14.988 17 12.493 17 9A7 7 0 103 9c0 3.492 1.698 5.988 3.355 7.584a13.731 13.731 0 002.273 1.765 11.842 11.842 0 00.976.544l.062.029.018.008.006.003zM10 11.25a2.25 2.25 0 100-4.5 2.25 2.25 0 000 4.5z" clip-rule="evenodd" />
+      </svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-5 h-5 animate-pulse">
+        <path fill-rule="evenodd" d="M9.69 18.933l.003.001C9.89 19.02 10 19 10 19s.11.02.308-.066l.002-.001.006-.003.018-.008a5.741 5.741 0 00.281-.14c.186-.096.446-.24.757-.433.62-.384 1.445-.966 2.274-1.765C15.302 14.988 17 12.493 17 9A7 7 0 103 9c0 3.492 1.698 5.988 3.355 7.584a13.731 13.731 0 002.273 1.765 11.842 11.842 0 00.976.544l.062.029.018.008.006.003zM10 11.25a2.25 2.25 0 100-4.5 2.25 2.25 0 000 4.5z" clip-rule="evenodd" />
+      </svg>`;
+  }, []);
+
+  // Add HTML marker
+  const addMarker = useCallback(
+    (htmlElement: string, currentMap: mapBoxGL.Map, coords: LngLatLike) => {
+      const el = document.createElement('div');
+      el.className = 'marker';
+      el.innerHTML = htmlElement;
+
+      el.onclick = flyToCurrentLocation;
+
+      // make a marker for each feature and add to the map
+      const marker = new mapBoxGL.Marker(el, {}).setLngLat(coords).addTo(currentMap);
+    },
+    [flyToCurrentLocation],
+  );
+
+  useEffect(() => {
+    map.current?.on('click', mouseEvent => {
+      if (map.current) {
+        addMarker(htmlElement, map.current, mouseEvent.lngLat);
+        console.log(mouseEvent.lngLat);
+      }
+    });
+  }, [addMarker, htmlElement]);
+
   const addPerformanceLayer = useCallback(() => {
     // Insert the layer beneath any symbol layer.
     const layers = map.current?.getStyle().layers;
@@ -98,9 +131,7 @@ const Map: FC<{
       layer => layer.type === 'symbol' && layer.layout?.['text-field'],
     )?.id;
 
-    // The 'building' layer in the Mapbox Streets
-    // vector tile set contains building height data
-    // from OpenStreetMap.
+    // The 'building' layer in the Mapbox Streets vector tile set contains building height data from OpenStreetMap.
     map.current?.addLayer(
       {
         id: 'add-3d-buildings',
@@ -303,8 +334,8 @@ export default Map;
 //       </svg>`;
 //
 // const animatedHtmlElement = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-//                 <path fill-rule="evenodd" d="M9.69 18.933l.003.001C9.89 19.02 10 19 10 19s.11.02.308-.066l.002-.001.006-.003.018-.008a5.741 5.741 0 00.281-.14c.186-.096.446-.24.757-.433.62-.384 1.445-.966 2.274-1.765C15.302 14.988 17 12.493 17 9A7 7 0 103 9c0 3.492 1.698 5.988 3.355 7.584a13.731 13.731 0 002.273 1.765 11.842 11.842 0 00.976.544l.062.029.018.008.006.003zM10 11.25a2.25 2.25 0 100-4.5 2.25 2.25 0 000 4.5z" clip-rule="evenodd" />
-//               </svg>`;
+//         <path fill-rule="evenodd" d="M9.69 18.933l.003.001C9.89 19.02 10 19 10 19s.11.02.308-.066l.002-.001.006-.003.018-.008a5.741 5.741 0 00.281-.14c.186-.096.446-.24.757-.433.62-.384 1.445-.966 2.274-1.765C15.302 14.988 17 12.493 17 9A7 7 0 103 9c0 3.492 1.698 5.988 3.355 7.584a13.731 13.731 0 002.273 1.765 11.842 11.842 0 00.976.544l.062.029.018.008.006.003zM10 11.25a2.25 2.25 0 100-4.5 2.25 2.25 0 000 4.5z" clip-rule="evenodd" />
+//       </svg>`;
 //
 // if (map.current && currentCoords) {
 //   addMarker(htmlElement, map.current, currentCoords);
