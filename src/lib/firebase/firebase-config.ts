8be8b25de915b550e0e4ff66a83f6a828375c381
julchu <jchumtl@gmail.com>
2023-10-22 15:04:33 -0400

    Updating Firebase emulator credentials

--- src/lib/firebase/firebase-config.ts
+++ src/lib/firebase/firebase-config.ts
@@ -25,11 +25,12 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0
 // const storage = getStorage(app);
 
 const firestore = getFirestore(app);
-// const firestoreAdmin = getF
 const authentication = getAuth(app);
 
-if (process.env.NEXT_PUBLIC_EMULATOR_ENABLED === 'TRUE') {
-  connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
-  connectAuthEmulator(authentication, 'http://127.0.0.1:9099', { disableWarnings: true });
+if (process.env.NEXT_PUBLIC_EMULATOR_ENABLED && process.env.NEXT_PUBLIC_LAN) {
+  connectFirestoreEmulator(firestore, process.env.NEXT_PUBLIC_LAN, 8080);
+  connectAuthEmulator(authentication, `http://${process.env.NEXT_PUBLIC_LAN}:9099`, {
+    disableWarnings: true,
+  });
 }
 export { firestore, authentication };
