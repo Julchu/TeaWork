aae86e1debb5e744437c63831a9429063246eda4
julchu <jchumtl@gmail.com>
2023-10-28 17:03:36 -0400

    Disabling inter for firebase preview test

--- src/components/ui/fonts.tsx
+++ src/components/ui/fonts.tsx
@@ -1,4 +1,4 @@
-import { Amatic_SC, Inter, Montserrat, Urbanist } from 'next/font/google';
+import { Amatic_SC, Montserrat, Urbanist } from 'next/font/google';
 
 export const montserrat = Montserrat({ subsets: ['latin'] });
 
@@ -12,7 +12,7 @@ export const amaticSC = Amatic_SC({
   weight: ['400', '700'],
 });
 
-export const inter = Inter({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800', '900'],
-});
+// export const inter = Inter({
+//   subsets: ['latin'],
+//   weight: ['400', '500', '600', '700', '800', '900'],
+// });
