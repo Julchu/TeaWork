728b7edc4cdf99172eedee457196cbe4d50e4bec
julchu <jchumtl@gmail.com>
2023-10-29 19:25:12 -0400

    Delete middleware, clean up settings

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,17 +1,9 @@
 import { FC } from 'react';
-import { cookies, headers } from "next/headers";
 
 const Settings: FC = () => {
-  const cookieStore = cookies();
-  const headerStore = headers();
-  console.log(cookieStore);
-  console.log(headerStore);
   return (
     // bg-gradient-to-r from-indigo-200 via-purple-500 to-pink-200
-    <main className="flex h-screen-small w-screen items-center justify-between p-6">
-      <div>{JSON.stringify(cookieStore)}</div>
-      <div>{JSON.stringify(headerStore)}</div>
-    </main>
+    <main className="flex h-screen-small w-screen items-center justify-between p-6"></main>
   );
 };
 export default Settings;
