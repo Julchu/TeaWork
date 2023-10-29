9e961ee9ae56fbe384d6296deeffcd9fdb0dabc6
julchu <jchumtl@gmail.com>
2023-10-29 00:27:22 -0400

    Getting cookies/headers

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,6 +1,11 @@
 import { FC } from 'react';
+import { cookies, headers } from "next/headers";
 
 const Settings: FC = () => {
+  const cookieStore = cookies();
+  const headerStore = headers();
+  console.log(cookieStore);
+  console.log(headerStore);
   return (
     // bg-gradient-to-r from-indigo-200 via-purple-500 to-pink-200
     <main className="flex h-screen-small w-screen items-center justify-between p-6">Settings</main>
