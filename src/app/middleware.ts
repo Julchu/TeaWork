b59a4dd875d5daf8e7f90a60078a0f9a01bb3395
julchu <jchumtl@gmail.com>
2023-10-29 15:41:53 -0400

    Testing redirect url

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -4,11 +4,12 @@ import { NextResponse } from 'next/server';
 // This function can be marked `async` if using `await` inside
 export const middleware = async (request: NextRequest) => {
   if (request.nextUrl.pathname.startsWith('/settings')) {
-    return NextResponse.rewrite(new URL('/other-examples', request.url));
+    // return NextResponse.rewrite(new URL('/other-examples', request.url));
+    return NextResponse.redirect(new URL('/other-examples', request.url));
   }
 };
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/',
+  matcher: '/settings',
 };
