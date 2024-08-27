4681d9ff4310be7ea32ea37aaea6ae28ab1553b7
julchu <jchumtl@gmail.com>
2024-08-27 16:02:11 -0400

    Filtering middleware for only GET requests

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,9 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getMiddleware } from 'src/app/get/getMiddleware';
 
 const middleware = async (request: NextRequest) => {
+  if (request.method !== 'GET') {
+    return;
+  }
   const response = NextResponse.next();
   if (request.nextUrl.pathname.startsWith('/')) await getMiddleware(request, response);
 
