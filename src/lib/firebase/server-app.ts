7d5a909215a5d02c2d26a0509aa689f0fb03bc72
julchu <jchumtl@gmail.com>
2024-08-27 00:50:41 -0400

    Cleaning variable

--- src/lib/firebase/server-app.ts
+++ src/lib/firebase/server-app.ts
@@ -16,7 +16,7 @@ export const getFirebaseServerApp = async () => {
     firebaseConfig,
     authIdToken
       ? {
-          authIdToken: authIdToken,
+          authIdToken,
         }
       : {},
   );
