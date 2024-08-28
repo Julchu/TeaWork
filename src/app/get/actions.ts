a108e784dc333630af09f355bb13fb224bdd8b8d
julchu <jchumtl@gmail.com>
2024-08-28 11:26:30 -0400

    Testing cookies token

--- src/app/get/actions.ts
+++ src/app/get/actions.ts
@@ -8,13 +8,7 @@ export async function create({ firstName, lastName }: { firstName: string; lastN
 
 export const setCookies = async (cookiesToAdd: { key: string; value: string }[]) => {
   cookiesToAdd.forEach(({ key, value }) => {
-    const expiresIn = 60 * 60 * 24 * 5 * 1000;
-    cookies().set(key, value, {
-      maxAge: expiresIn,
-      httpOnly: true,
-      secure: true,
-      sameSite: 'strict',
-    });
+    cookies().set(key, value);
   });
 };
 
